1fb7b85f99f87cd82cfcbb234952e175e658c2fb
jjh75607 <dkrak5738@naver.com>
2020-10-21 13:22:11 +0900

    mid

--- c언어/2학기/06.c
+++ c언어/2학기/06.c
@@ -31,49 +31,49 @@ int main() {
     }
     
 //    case 2
-    int W[4] = { 0 };
-    for (int i = 0, j = 1, status = 0; searchName[j] != '\0'; ++j)
-    {
-        if (searchName[i] == searchName[j])
-        {
-            i++;
-            W[j] = ++status;
-        }
-        else if (i > 0)
-        {
-            i = 0;
-            W[j] = status = 0;
-        }
-    }
-    
-    //
-    for (int i = 0; i < sizeof(names) / sizeof(names[0]); i++) {
-        for (int s = 0, f = 0; s < strlen(names[i]);)
-        {
-            while (names[i][s] == searchName[f] && f < strlen(searchName))
-            {
-                s++;
-                f++;
-            }
-
-            if (f > strlen(searchName) - 1)
-            {
-                for (int k = 0; k < strlen(names[i]); k++) {
-                    printf("%c", names[i][k]);
-                }
-                f = W[f - 1];
-            }
-            else
-            {
-                if (W[f] == 0)
-                    f = 0;
-                else if (W[f] > 0)
-                    f = W[f] - 1;
-                s++;
-            }
-        }
-        printf("\n");
-    }
+//    int W[4] = { 0 };
+//    for (int i = 0, j = 1, status = 0; searchName[j] != '\0'; ++j)
+//    {
+//        if (searchName[i] == searchName[j])
+//        {
+//            i++;
+//            W[j] = ++status;
+//        }
+//        else if (i > 0)
+//        {
+//            i = 0;
+//            W[j] = status = 0;
+//        }
+//    }
+//
+//    //
+//    for (int i = 0; i < sizeof(names) / sizeof(names[0]); i++) {
+//        for (int s = 0, f = 0; s < strlen(names[i]);)
+//        {
+//            while (names[i][s] == searchName[f] && f < strlen(searchName))
+//            {
+//                s++;
+//                f++;
+//            }
+//
+//            if (f > strlen(searchName) - 1)
+//            {
+//                for (int k = 0; k < strlen(names[i]); k++) {
+//                    printf("%c", names[i][k]);
+//                }
+//                f = W[f - 1];
+//            }
+//            else
+//            {
+//                if (W[f] == 0)
+//                    f = 0;
+//                else if (W[f] > 0)
+//                    f = W[f] - 1;
+//                s++;
+//            }
+//        }
+//        printf("\n");
+//    }
     
     return 0;
 }
